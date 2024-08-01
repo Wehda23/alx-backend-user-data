3c864172180bb0545d2a6566ff6b929f3c950951
Waheed Khaled <waheedkhaled95@gmail.com>
2024-08-02 02:17:49 +0300

    Test file for ALX checkers

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,13 +15,15 @@ def filter_datum(
     separator: str,
 ) -> str:
     """
-    This function filters the given message by replacing the specified fields with the replacer string.
+    This function filters the given message by replacing the specified\
+        fields with the replacer string.
 
     Args:
         fields (str): A string containing the fields to be replaced.
         replacer (str): The string to replace the fields with.
         message (str): The message to be filtered.
-        separator (str): The separator used to separate the fields in the fields string.
+        separator (str): The separator used to separate \
+            the fields in the fields string.
 
     Returns:
         str: The filtered message.
